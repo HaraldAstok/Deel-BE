b3e3ea33535d4f7158df13688e049f7a2ce8f5f3
HaraldAstok <harald.astok@pipedrive.com>
2022-11-16 12:19:11 +0200

    implement contracts/ endpoint

--- src/app.js
+++ src/app.js
@@ -1,25 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const { sequelize } = require('./model');
+const { getProfile } = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
 
 /**
- *
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
+app.get('/contracts/:id', getProfile, async (req, res) => {
+	const { Contract } = req.app.get('models');
+	const { id } = req.params;
 
-    const { id: clientId } = req.profile.dataValues;
+	const { id: clientId } = req.profile.dataValues;
 
-    const contract = await Contract.findOne({where: {id, clientId}})
-    if (!contract) return res.status(404).send({ message: 'no contracts found', status: 404 }).end();
+	const contract = await Contract.findOne({ where: { id, clientId } });
+	if (!contract) return res.status(404).send({ message: 'no contracts found', status: 404 }).end();
 
-    res.json(contract)
-})
+	res.json(contract);
+});
+
+/**
+ * @returns all non terminated contracts
+ */
+app.get('/contracts', getProfile, async (req, res) => {
+	const { Contract } = req.app.get('models');
+
+	const { id: clientId } = req.profile.dataValues;
+	const contracts = await Contract.findAll({
+		where: {
+			[Op.or]: [{ clientId }, { contractorId: clientId }],
+			status: {
+				[Op.not]: 'terminated',
+			},
+		},
+	});
+	if (!contracts) return res.status(404).send({ message: 'no contracts found', status: 404 }).end();
+	res.json(contracts);
+});
+
 module.exports = app;
