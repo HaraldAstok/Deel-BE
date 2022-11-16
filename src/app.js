cffa6af3b9cbdaa1dd980f3846d396d7c59b027d
HaraldAstok <harald.astok@pipedrive.com>
2022-11-16 12:06:23 +0200

    fix contracts/:id endpoint

--- src/app.js
+++ src/app.js
@@ -8,14 +8,18 @@ app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ *
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
+
+    const { id: clientId } = req.profile.dataValues;
+
+    const contract = await Contract.findOne({where: {id, clientId}})
+    if (!contract) return res.status(404).send({ message: 'no contracts found', status: 404 }).end();
+
     res.json(contract)
 })
 module.exports = app;
